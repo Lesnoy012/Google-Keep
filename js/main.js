e0aca359b9e9a11f4f397be613d8fdf1be4384dc
Vlad Ipatov <royalevlad2@gmail.com>
2024-09-22 17:31:50 +0300

    end version Vlad

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 const formAddNote = document.querySelector("#formAddNote");
 const inputNewNote = document.querySelector("#form__input");
-const arrNotes = [];
-
-
-formAddNote.addEventListener('submit', (event)=>{
-    event.preventDefault();
-    arrNotes.push(new NewNote(inputNewNote.value));
-    console.log(arrNotes);
-    formAddNote.reset();
-})
+const cardsForm = document.querySelector("#cardsForm");
 
 class NewNote {
     constructor(title = '', text = '', date = '') {
@@ -18,24 +10,60 @@ class NewNote {
     }
 }
 
-// arrNotes.push(new NewNote('Промисы JS', 'Прочитать документацию', '01.12.2005'));
+const arrNotes = [new NewNote("Callback", "Прочитать док-ию", new Date ())];
 
-// console.log(arrNotes[0].date);
-// let html = `<div class="col">
-//               <div class="card h-100">
-//                 <div class="card-body">
-//                   <h5 class="card-title">Card title</h5>
-//                   <p class="card-text">
-//                     This is a wider card with supporting text below as a natural
-//                     lead-in to additional content. This card has even longer
-//                     content than the first to show that equal height action.
-//                   </p>
-//                 </div>
-//                 <div class="card-footer">
-//                   <small class="text-body-secondary" 
-//                     >Last updated 3 mins ago</small
-//                   >
-//                 </div>
-//               </div>
-//             </div>`;
+//Рендерим все карточки, уже находящиеся в массиве.
+arrNotes.forEach((item) => {
+    let cardHTML = `<div class="col">
+              <div class="card h-100">
+                <div class="card-body">
+                  <h5 class="card-title">${item.title}</h5>
+                  <p class="card-text">
+                  ${item.text}
+                  </p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-body-secondary">
+                  ${item.date}
+                  </small>
+                </div>
+              </div>
+            </div>`
+    cardsForm.insertAdjacentHTML("afterbegin", cardHTML);
+})
 
+formAddNote.addEventListener('submit', (event)=>{
+    // Добавляем новую карточку в массив
+    event.preventDefault();
+    arrNotes.push(new NewNote(inputNewNote.value, '', new Date ()));
+    formAddNote.reset();
+
+    // Рендерим новую карточку на странице, добавляя её в начало
+    const lastCardIndex = arrNotes.length - 1;
+
+    let cardHTML = `<div class="col">
+              <div class="card h-100">
+                <div class="card-body"> 
+                  <h5 class="card-title">${arrNotes[lastCardIndex].title}</h5>
+                  <input class="card-text" id = "inputCardText${lastCardIndex}">
+                  ${arrNotes[lastCardIndex].text}
+                  </input>
+                </div>
+                <div class="card-footer">
+                  <small class="text-body-secondary">
+                  ${new Date - arrNotes[lastCardIndex].date}
+                  minutes ago
+                  </small>
+                </div>
+              </div>
+            </div>`
+    cardsForm.insertAdjacentHTML("afterbegin", cardHTML);
+    const inputCardText = document.querySelector(`#inputCardText${lastCardIndex}`);
+    inputCardText.focus();
+
+    inputCardText.addEventListener("keydown", (event)=>{
+        if(event.keyCode == 13) {
+            inputCardText.disabled = true;
+        }
+    })
+})
